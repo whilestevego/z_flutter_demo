3adb4be2057654f1ee8d962fc872a6b2d6e75f64
Steve Godin <steve.god@gmail.com>
2020-03-12 10:30:44 -0700

    Add border prop to Pill

--- lib/components/pill.dart
+++ lib/components/pill.dart
@@ -4,25 +4,29 @@ class Pill extends StatelessWidget {
   final String text;
   final Color color;
   final Color backgroundColor;
+  final BoxBorder border;
 
   const Pill({
     Key key,
     @required this.text,
     this.color = Colors.white,
     this.backgroundColor = Colors.grey,
+    this.border = const Border(),
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        decoration: BoxDecoration(
-          color: backgroundColor,
-          borderRadius: BorderRadius.all(Radius.circular(10)),
-        ),
-        padding: EdgeInsets.fromLTRB(10, 2, 10, 2),
-        child: Text(
-          text,
-          style: TextStyle(color: color),
-        ));
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        border: border,
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+      ),
+      padding: EdgeInsets.fromLTRB(10, 2, 10, 2),
+      child: Text(
+        text,
+        style: TextStyle(color: color),
+      ),
+    );
   }
 }
